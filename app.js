c1b0713f8c52e3bb55bdb9e110890d4b0327f7d3
samar-99 <samarmbarek19@gmail.com>
2025-01-12 19:48:26 +0100

    main

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 require('./models/index');
+const cors= require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
